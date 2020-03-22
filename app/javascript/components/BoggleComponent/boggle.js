e248700d9128190ae97727a764bb6255dd8162a8
Kushal Shrestha <i213300@d2hs.com>
2020-03-23 00:26:34 +0545

    Calculated Cumulative Frequency for alphabets

--- app/javascript/components/BoggleComponent/boggle.js
+++ app/javascript/components/BoggleComponent/boggle.js
@@ -1,9 +1,8 @@
 import React from "react";
-
 import BoggleSquare from './BoggleSquareComponent/boggle-square';
-
 import {Container, Row, Col} from "react-bootstrap";
-
+import './boggle.css';
+ import mytext from './letter-frequency.json';
 
 export default class Board extends React.Component{
 
@@ -11,8 +10,43 @@ export default class Board extends React.Component{
         super(props);
         this.state={
             box_size: 4,
+            alphabet_distribution:[],
+            sum:0,
+            isDataLoaded: false
         }
     };
+
+    componentDidMount(){
+        this.importReferenceTableValue();
+    }
+
+    importReferenceTableValue(){
+        console.log('here');
+        var cumulativefrequency=0;
+        var obj={};
+        var alphabetList=[];
+
+        mytext.EnglishRelativeFrequency.map((letterDetail,i)=>{
+                    cumulativefrequency+=letterDetail.frequency;
+                    obj={'letter':letterDetail.letter, 'frequency':letterDetail.frequency,'cumulativefrequency':cumulativefrequency};
+                    alphabetList.push(obj);
+                    obj={}
+                });
+
+        this.setState(()=>{
+                    return{
+                        alphabet_distribution: alphabetList,
+                        sum: cumulativefrequency,
+                        isDataLoaded: true
+                    }
+                },()=>{
+                    console.log(this.state.alphabet_distribution);
+                }
+                );
+
+                
+    }
+
 
     createBoggleBoard=()=>{
         console.log('Creating Boggle Board');
@@ -22,7 +56,7 @@ export default class Board extends React.Component{
             for(let j=0;j<this.state.box_size;j++){
             boardColumn.push(<BoggleSquare key={j}/>);
             }
-            boardRow.push(boardColumn);
+            boardRow.push(<div className="board-row" key={i}>{boardColumn}</div>);
         }    
         
         return <div>{boardRow}</div>;
@@ -33,7 +67,7 @@ export default class Board extends React.Component{
             <div>
                 <Container>
                     <Row>
-                        <Col xs={5}>
+                        <Col xs={5} className="board">
                             {this.createBoggleBoard()}
                         </Col>
                     </Row>
