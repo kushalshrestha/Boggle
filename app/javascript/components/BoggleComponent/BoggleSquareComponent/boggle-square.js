4e75a4e8d4280ec778529631681f00524f28ddf3
Kushal Shrestha <i213300@d2hs.com>
2020-03-25 13:02:21 +0545

    Alphabet validation RULES SETUP completed

--- app/javascript/components/BoggleComponent/BoggleSquareComponent/boggle-square.js
+++ app/javascript/components/BoggleComponent/BoggleSquareComponent/boggle-square.js
@@ -6,9 +6,9 @@ import './boggle-square.css';
 export default class BoggleSquare extends React.Component{
     render(){
         return(
-            <div>
+            
                 <button className="square">{this.props.alphabet}</button>
-            </div>
+            
         )
     }
 }
--- app/javascript/components/BoggleComponent/boggle.js
+++ app/javascript/components/BoggleComponent/boggle.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BoggleSquare from './BoggleSquareComponent/boggle-square';
-import {Container, Row, Col} from "react-bootstrap";
+import InputField from './InputComponent/input-field';
+import {Container, Row, Col,Button} from "react-bootstrap";
 import './boggle.css';
  import mytext from './letter-frequency.json';
 
@@ -9,12 +10,14 @@ export default class Board extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            box_size: 4,
+            box_size: 7,
             alphabet_distribution:[],
             sum:0,
             isDataLoaded: false,
             selected_alphabets:'',
-            isAlphabetGenerated: false
+            isAlphabetGenerated: false,
+            isPossibleMoveCalculated: false,
+            possibleMoves: {}
         }
     };
 
@@ -87,99 +90,147 @@ export default class Board extends React.Component{
 
     generatePossibleMoves(){
         var available_characters = this.state.selected_alphabets;
-        var possiblities=[];
-        var character_moves=[{
-            'current_value':'',
-            'right':'',
-            'bottom_right':'',
-            'bottom':'',
-            'top':'',
-            'top_right':''
-    }];
-        
+        var possiblities={};
         var box_size=this.state.box_size;
     
-        console.log('Available in board: '+available_characters);
-    
         for(var i=0; i<available_characters.length; i++){
+            var possible_move_index=[];
+            var index_detail={};
             var current_row = Math.floor(i/box_size);
             
-             this.calculateMoveRight(i, current_row, box_size, available_characters);
-             this.calculateMoveBottom(i, current_row, box_size, available_characters);
-             this.calculateMoveLeft(i, current_row, box_size, available_characters);
-             this.calculateMoveTop(i, current_row, box_size, available_characters);
-             this.calculateMoveBottomRight(i, current_row, box_size, available_characters);
-             this.calculateMoveBottomLeft(i, current_row, box_size, available_characters);
-             this.calculateMoveTopLeft(i, current_row, box_size, available_characters);
-            this.calculateMoveTopRight(i, current_row, box_size, available_characters);
+            
+            var right = this.calculateMoveRight(i, current_row, box_size, available_characters);
+            var bottom =  this.calculateMoveBottom(i, current_row, box_size, available_characters);
+            var left =  this.calculateMoveLeft(i, current_row, box_size, available_characters);
+            var top =  this.calculateMoveTop(i, current_row, box_size, available_characters);
+            var bottom_right = this.calculateMoveBottomRight(i, current_row, box_size, available_characters);
+            var bottom_left =  this.calculateMoveBottomLeft(i, current_row, box_size, available_characters);
+            var top_left =  this.calculateMoveTopLeft(i, current_row, box_size, available_characters);
+            var top_right =  this.calculateMoveTopRight(i, current_row, box_size, available_characters);
+        
+            if(right!=null){
+                possible_move_index.push(right);
+            }
+            if(bottom!=null){
+                possible_move_index.push(bottom);
+            }
+            if(left!=null){
+                possible_move_index.push(left);
+            }
+            if(top!=null){
+                possible_move_index.push(top);
+            }
+            if(bottom_right!=null){
+                possible_move_index.push(bottom_right);
+            }
+            if(bottom_left!=null){
+                possible_move_index.push(bottom_left);
+            }
+            if(top_left!=null){
+                possible_move_index.push(top_left);
+            }
+            if(top_right!=null){
+                possible_move_index.push(top_right);
+            }
+            // console.log("for i: "+i+ " Possible moves: "+possible_move_index);
+            index_detail={"value": available_characters.charAt(i),
+                         "possible_moves": possible_move_index};
+            
+            possiblities[i]=index_detail;
             
         }
+        console.log(JSON.stringify(possiblities));
+        this.setState(()=>{
+            return{
+                possibleMoves: possiblities,
+                isPossibleMoveCalculated: true
+            }
+        },()=>{
+            console.log(this.state.possibleMoves);
+        }
+        );
 
     }
 
     calculateMoveRight(i, current_row, box_size, available_characters){
         var target_index=i+1;
         var target_row=Math.floor(target_index/box_size);
-        console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
+        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==0 && target_index<Math.pow(box_size,2)){
-            console.log("RIGHT "+ available_characters.charAt(target_index));
+            // console.log("RIGHT "+ available_characters.charAt(target_index));
+            return target_index;
         }
+        return null;
     }
     calculateMoveBottom(i, current_row, box_size, available_characters){
         var target_index=i+box_size;
         var target_row=Math.floor(target_index/box_size);
-        console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
+        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==1 && target_index<Math.pow(box_size,2)){
-            console.log("BOTTOM "+ available_characters.charAt(target_index));
+            // console.log("BOTTOM "+ available_characters.charAt(target_index));
+            return target_index;
         }
+        return null;
     }
     calculateMoveLeft(i, current_row, box_size, available_characters){
         var target_index=i-1;
         var target_row=Math.floor(target_index/box_size);
-        console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
+        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==0 && target_index>=0){
-            console.log("LEFT "+ available_characters.charAt(target_index));
+            // console.log("LEFT "+ available_characters.charAt(target_index));
+            return target_index;
         }
+        return null;
     }
     calculateMoveTop(i, current_row, box_size, available_characters){
         var target_index=i-box_size;
         var target_row=Math.floor(target_index/box_size);
-        console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
+        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((current_row-target_row)==1 && target_index>=0){
-            console.log("Top "+ available_characters.charAt(target_index));
+            // console.log("Top "+ available_characters.charAt(target_index));
+            return target_index;
         }
+        return null;
     }
     calculateMoveBottomRight(i, current_row, box_size, available_characters){
         var target_index=i+(box_size+1);
         var target_row=Math.floor(target_index/box_size);
-        console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
+        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==1 && target_index<Math.pow(box_size,2)){
-            console.log("Bottom Right "+ available_characters.charAt(target_index));
+            // console.log("Bottom Right "+ available_characters.charAt(target_index));
+            return target_index;
         }
+        return null;
     }
     calculateMoveBottomLeft(i, current_row, box_size, available_characters){
         var target_index=i+(box_size-1);
         var target_row=Math.floor(target_index/box_size);
-        console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
+        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==1 && target_index<Math.pow(box_size,2)){
-            console.log("Bottom Left "+ available_characters.charAt(target_index));
+            // console.log("Bottom Left "+ available_characters.charAt(target_index));
+            return target_index;
         }
+        return null;
     }
     calculateMoveTopLeft(i, current_row, box_size, available_characters){
         var target_index=i-(box_size+1);
         var target_row=Math.floor(target_index/box_size);
-        console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
+        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((current_row-target_row)==1 && target_index>=0){
-            console.log("Top Left "+ available_characters.charAt(target_index));
+            // console.log("Top Left "+ available_characters.charAt(target_index));
+            return target_index;
         }
+        return null;
     }
     calculateMoveTopRight(i, current_row, box_size, available_characters){
         var target_index=i-(box_size-1);
         var target_row=Math.floor(target_index/box_size);
-        console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
+        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((current_row-target_row)==1 && target_index>=0){
-            console.log("Top Right "+ available_characters.charAt(target_index));
+            // console.log("Top Right "+ available_characters.charAt(target_index));
+            return target_index;
         }
+        return null;
     }
 
     
@@ -211,9 +262,13 @@ export default class Board extends React.Component{
                 <div>
                     <Container>
                         <Row>
-                            <Col xs={5} className="board">
+                            <Col xs={8} className="">
                                 {this.createBoggleBoard()}
                             </Col>
+                            
+                            <Col xs={4}><InputField/><Button variant="primary">Primary</Button></Col>
+
+                            
                         </Row>
                     </Container>
                 
