7ec441865f3b57841ddc6851e2a113981f323926
kushalshrestha <kushalshr@gmail.com>
2020-03-27 01:09:22 +0545

    Input field with Timer Component and disable after timeout

--- app/javascript/components/BoggleComponent/InputComponent/input-field.js
+++ app/javascript/components/BoggleComponent/InputComponent/input-field.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './input-field.css';
 
+import Timer from './../TimerComponent/timer';
+
 export default class InputField extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            value:''
+            value:'',
+            disable_input : false
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,6 +23,9 @@ export default class InputField extends React.Component{
     handleSubmit(event){
         this.checkData(this.state.value);
         // alert(' A name was submitted : '+this.state.value);
+        this.setState({
+          value: ''
+        });
         event.preventDefault();
     }
 
@@ -27,14 +33,27 @@ export default class InputField extends React.Component{
       this.props.parentCallback(value);
     }
 
+    callbackFunction = (childData) => {
+      console.log("CHILD DATA : "+childData);
+      this.setState({
+        disable_input : childData
+      });
+    }
+
     render() {
         return (
           <form onSubmit={this.handleSubmit}>
             <h4>Enter a word: </h4>
             <label>
-              <input className="input-lg" type="text" value={this.state.value} onChange={this.handleChange} />
+              <input className="input-lg"
+                    // (this.state.disable_input?"disabled":"")
+                    // className={"input-lg " +(this.state.disable_input?"disabled":"")} 
+                    disabled={this.state.disable_input}
+                    type="text" value={this.state.value} onChange={this.handleChange} />
             </label>
             {/* <input type="submit" value="Submit" /> */}
+
+            <h5><Timer disableCheck={this.callbackFunction}/></h5>
           </form>
         );
       }
