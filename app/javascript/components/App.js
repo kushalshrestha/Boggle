28ac59e5829c6457afaed45a364098a62df8fe79
kushalshrestha <kushalshr@gmail.com>
2020-04-01 11:50:12 +0545

    Renamed HelloWorld to App.js, code cleanup

---
+++ app/javascript/components/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navigation from './BoggleComponent/NavComponent/navigation';
+
+
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Navigation></Navigation>
+        </React.Fragment>
+    );
+  }
+}
+
+export default App
