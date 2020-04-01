bec271f7308ac4fea5535a5539a04153aef56649
kushalshrestha <kushalshr@gmail.com>
2020-04-01 11:46:28 +0545

    Code cleanup with notification and api request on different file

--- app/javascript/components/BoggleComponent/boggle.js
+++ app/javascript/components/BoggleComponent/boggle.js
@@ -7,8 +7,10 @@ import {Container, Row, Col,Button, Alert,ButtonToolbar, ButtonGroup, Card} from
 import './boggle.css';
 import mytext from './letter-frequency.json';
 
-import {ToastContainer, toast} from 'react-toastify';
+import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import APIRequestService from './Engine/service';
+import {notify} from './Engine/toast';
 
 export default class Board extends React.Component{
 
@@ -60,18 +62,14 @@ export default class Board extends React.Component{
         
         var boggle_alphabets = this.state.selected_alphabets;
         var possible_movesList = this.state.possibleMoves;
-        // alert('you entered the word : '+word);
-        console.log("WORD.CHARAT(0) :"+word.charAt(0));
+        
         var first_word = word.charAt(0);
         var match_pos=[];
 
         
         //finding possible indexes of starting word
-
-        
-
         for(var i=0,j=(boggle_alphabets.length-1);i<boggle_alphabets.length;i++,j--){
-            // console.log(i);
+            
             if(i>=j){
                 if(boggle_alphabets.charAt(i)==first_word){
                     match_pos.push(i);
@@ -90,14 +88,12 @@ export default class Board extends React.Component{
 
         
         var result = this.checkTraverseValidity(1,match_pos,possible_movesList,boggle_alphabets,word);
-        console.log("RESULT: "+result);
         
-        if(result===true){
+        if(result===true ){
             this.fetchAPIResponse(word);
         }
-        if(result===false){
-            
-            this.notifyError(word,"0");
+        if(result===false & word.length>1){
+            notify(word,null);
         }
 
 
@@ -105,97 +101,36 @@ export default class Board extends React.Component{
     }
 
     fetchAPIResponse(word){
-        console.log('API called');
-
-        fetch('/welcome/post', {
-             method: 'post',
-             headers: {'Content-Type':'application/json'},
-             body: JSON.stringify({
-              word: word
-             })
-        }).then(async response => {
-            
-            const data = await response.json();
-            console.log(JSON.stringify(response));
-            if(!response.ok){
-                const error=(data && data.message);
-                return Promise.reject(error);
-            }
-            if(word.length>=1){
-                console.log("CALLED YO YO");
-                this.setState({
-                    valid_word : word
-                });
-            }
-            this.notifySuccess(word);
-
-        })
-        .catch(error =>{
-            console.log('ERROR : '+error);
-            this.notifyError(word,"1");
-        })
-
-
-        // fetch('https://api.dictionaryapi.dev/api/v1/entries/en/'+word)
-        // .then(async response => {
-            
-        //     const data = await response.json();
-        //     console.log(JSON.stringify(response));
-        //     if(!response.ok){
-        //         const error=(data && data.message);
-        //         return Promise.reject(error);
-        //     }
-        //     if(word.length>=1){
-        //         console.log("CALLED YO YO");
-        //         this.setState({
-        //             valid_word : word
-        //         });
-        //     }
-        //     this.notifySuccess(word);
-
-        // })
-        // .catch(error =>{
-        //     console.log('ERROR : '+error);
-        //     this.notifyError(word,"1");
-        // })
-        
+        this.apiRequestService = new APIRequestService(word);
+        this.apiRequestService.fetchAPIResponse().then(async response => {
+           const data = await response;
+           if(data!=200){
+               const error=(data && data.message);
+               return Promise.reject(error);
+           }
+           if(word.length>1){
+               this.setState({
+                   valid_word : word
+               });
+               notify(word,0);
+           }
+           
+       })
+       .catch(error =>{
+           notify(word,1);
+       })
     }
 
-    notifySuccess(word){
-        toast.success(word + " -  is a valid word.",{position: toast.POSITION.BOTTOM_RIGHT});
-    }
-    
-
-    notifyError(word,id){
-        
-        if(id==1){
-            toast.error(word + " -  is not a valid word",{position: toast.POSITION.BOTTOM_RIGHT});
-        }
-        else {
-            toast.error(word + " -  cannot be created from the board",{position: toast.POSITION.BOTTOM_RIGHT});
-        }
-    }
-
-    
     checkTraverseValidity(position,match_position,possible_movesList,boggle_alphabets,entered_word){
-        // alert("POSITION: "+position);
-        // alert("Match position "+JSON.stringify(match_position));
-        // alert("boggle alphabet "+boggle_alphabets);
-        // alert("entered word "+entered_word);
-        console.log(position+"st/nd/th letter in entered word matches on following positions: "+JSON.stringify(match_position));
-        
-        var match_position_next=[];
+        // console.log(position+"st/nd/th letter in entered word matches on following positions: "+JSON.stringify(match_position));
+       var match_position_next=[];
         
         for(var j=0;j<match_position.length;j++){
-            console.log("LOOP : "+j+" ARRAY: "+match_position);
             var index=match_position[j];
-            console.log("POSSIBLE MOVE index "+ index );
             for(var k=0;k<possible_movesList[index].possible_moves.length;k++){
                 if(entered_word.charAt(position)==boggle_alphabets.charAt(possible_movesList[index].possible_moves[k])){
                     match_position_next.push(possible_movesList[index].possible_moves[k]);
-                    console.log(j+'st/nd/th element on the above array'+
-                                'i.e index position on board : '+index+' || has next character matched on : '+k+ '|| ARRAY: '+JSON.stringify(match_position));
-           }
+                }
             }
             
         }
@@ -209,8 +144,6 @@ export default class Board extends React.Component{
         }
 
     }
-
-    
 
     importReferenceTableValue(){
         console.log('here');
@@ -232,10 +165,7 @@ export default class Board extends React.Component{
                         isDataLoaded: true
                     }
                 },()=>{
-                    console.log(this.state.alphabet_distribution);
-                
                     this.generateAlphabets();
-                    console.log('generateAlphabets called')
                 }
                 );
 
@@ -258,7 +188,6 @@ export default class Board extends React.Component{
                    
                     break;
                 }
-                console.log("SELECTED "+selected);
             }
         }
 
@@ -268,7 +197,6 @@ export default class Board extends React.Component{
                 isAlphabetGenerated : true
             }
         },()=>{
-            console.log("GENERATE POSSIBLE MOVESads");
             this.generatePossibleMoves();
             
         }
@@ -279,10 +207,8 @@ export default class Board extends React.Component{
         var available_characters = this.state.selected_alphabets;
         var possiblities={};
         let box_size=parseInt(this.state.box_size);
-        // alert('HERE '+typeof(box_size));
     
         for(var i=0; i<available_characters.length; i++){
-            // if(i==0){
             var possible_move_index=[];
             var index_detail={};
             var current_row = Math.floor(i/box_size);
@@ -296,12 +222,7 @@ export default class Board extends React.Component{
             var bottom_left =  this.calculateMoveBottomLeft(i, current_row, box_size, available_characters);
             var top_left =  this.calculateMoveTopLeft(i, current_row, box_size, available_characters);
             var top_right =  this.calculateMoveTopRight(i, current_row, box_size, available_characters);
-            
-            // alert(right+","+bottom+","+
-            //             left+","+top+","+bottom_right+
-            //             ","+bottom_left+","+top_left+","+
-            //             top_right);
-
+    
             if(right!=null){
                 possible_move_index.push(right);
             }
@@ -326,14 +247,12 @@ export default class Board extends React.Component{
             if(top_right!=null){
                 possible_move_index.push(top_right);
             }
-            // console.log("for i: "+i+ " Possible moves: "+possible_move_index);
             index_detail={"value": available_characters.charAt(i),
                          "possible_moves": possible_move_index};
             
             possiblities[i]=index_detail;
-        // }   
         }
-        console.log(JSON.stringify(possiblities));
+    
         this.setState(()=>{
             return{
                 possibleMoves: possiblities,
@@ -349,16 +268,7 @@ export default class Board extends React.Component{
     calculateMoveRight(i, current_row, box_size, available_characters){
         var target_index=i+1;
         var target_row=Math.floor(target_index/box_size);
-        // alert(box_size);
-        // alert(typeof(box_size));
-        // alert(i);
-        // alert(typeof(i));
-        // alert(target_index);
-        // alert(typeof(target_index));
-        // alert(target_row);
-        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==0 && target_index<Math.pow(box_size,2)){
-            // console.log("RIGHT "+ available_characters.charAt(target_index));
             return target_index;
         }
         return null;
@@ -366,16 +276,7 @@ export default class Board extends React.Component{
     calculateMoveBottom(i, current_row, box_size, available_characters){
         var target_index=i+box_size;
         var target_row=Math.floor(target_index/box_size);
-        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
-        // alert(box_size);
-        // alert(typeof(box_size));
-        // alert(i);
-        // alert(typeof(i));
-        // alert(target_index);
-        // alert(typeof(target_index));
-        // alert(target_row);
         if((target_row-current_row)==1 && target_index<Math.pow(box_size,2)){
-            // console.log("BOTTOM "+ available_characters.charAt(target_index));
             return target_index;
         }
         return null;
@@ -383,9 +284,7 @@ export default class Board extends React.Component{
     calculateMoveLeft(i, current_row, box_size, available_characters){
         var target_index=i-1;
         var target_row=Math.floor(target_index/box_size);
-        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==0 && target_index>=0){
-            // console.log("LEFT "+ available_characters.charAt(target_index));
             return target_index;
         }
         return null;
@@ -393,9 +292,7 @@ export default class Board extends React.Component{
     calculateMoveTop(i, current_row, box_size, available_characters){
         var target_index=i-box_size;
         var target_row=Math.floor(target_index/box_size);
-        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((current_row-target_row)==1 && target_index>=0){
-            // console.log("Top "+ available_characters.charAt(target_index));
             return target_index;
         }
         return null;
@@ -403,9 +300,7 @@ export default class Board extends React.Component{
     calculateMoveBottomRight(i, current_row, box_size, available_characters){
         var target_index=i+(box_size+1);
         var target_row=Math.floor(target_index/box_size);
-        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==1 && target_index<Math.pow(box_size,2)){
-            // console.log("Bottom Right "+ available_characters.charAt(target_index));
             return target_index;
         }
         return null;
@@ -413,9 +308,7 @@ export default class Board extends React.Component{
     calculateMoveBottomLeft(i, current_row, box_size, available_characters){
         var target_index=i+(box_size-1);
         var target_row=Math.floor(target_index/box_size);
-        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((target_row-current_row)==1 && target_index<Math.pow(box_size,2)){
-            // console.log("Bottom Left "+ available_characters.charAt(target_index));
             return target_index;
         }
         return null;
@@ -423,9 +316,7 @@ export default class Board extends React.Component{
     calculateMoveTopLeft(i, current_row, box_size, available_characters){
         var target_index=i-(box_size+1);
         var target_row=Math.floor(target_index/box_size);
-        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((current_row-target_row)==1 && target_index>=0){
-            // console.log("Top Left "+ available_characters.charAt(target_index));
             return target_index;
         }
         return null;
@@ -433,10 +324,8 @@ export default class Board extends React.Component{
     calculateMoveTopRight(i, current_row, box_size, available_characters){
         var target_index=i-(box_size-1);
         var target_row=Math.floor(target_index/box_size);
-        // console.log('i : '+i+' Target Row : '+target_row+' Current Row : '+current_row+' Target Index : '+target_index);
         if((current_row-target_row)==1 && target_index>=0){
-            // console.log("Top Right "+ available_characters.charAt(target_index));
-            return target_index;
+             return target_index;
         }
         return null;
     }
