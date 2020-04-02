a8a5998cad73f9e074be3eecf51c239873ea3e72
kushalshrestha <kushalshr@gmail.com>
2020-04-02 10:36:35 +0545

    Rename nav option

--- app/javascript/components/BoggleComponent/NavComponent/navigation.js
+++ app/javascript/components/BoggleComponent/NavComponent/navigation.js
@@ -11,7 +11,7 @@ class Navigation extends React.Component {
                 <nav className="navbar navbar-expand-lg navbar-light nav">
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto nav__item nav__list">
-                    <li className="nav-item nav__link"><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+                    <li className="nav-item nav__link"><NavLink to="/" exact activeClassName="active">Boggle</NavLink></li>
                     {/* <li className="nav-item nav__link"><NavLink to="/highscore" exact activeClassName="active">HighScore</NavLink></li> */}
                     </ul>
                     </div>
